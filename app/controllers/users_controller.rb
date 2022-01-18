65a1cfdd76605b50c902a42ac6ac5fe3cf1a9abb
le-murier <vofimovic@gmail.com>
2022-01-18 04:20:19 +0200

    was added routes for user model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def show
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.new({
+          user_name: params[:user_name],
+          email: params[:email],
+          password: params[:password],
+          admin: params[:admin]
+      })
+    if @user.save
+      render json: @user
+    else
+      render error: {error: "Unable to create a user"}, status: 400
+    end
+  end
+
+  def delete
+    @user = User.find(params[:id])
+    @user.destroy
+    render json: {message: "User was destroyed"}, status: 200
+  end
+
+end
